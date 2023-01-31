2c163778c51248bdeb1f255a05e1ab58f3e2c8dd
s21484-pj <s21484@pjwstk.edu.pl>
2023-01-31 20:08:24 +0100

    initialize project

---
+++ src/test/java/pl/pelikan/pelikanbe/PelikanBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pelikan.pelikanbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelikanBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
